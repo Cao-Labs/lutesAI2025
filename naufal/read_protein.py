7e47531f76e06c51a9ee6304d8b609e4f4e28f4e
Naufal Alavi <naufal.alavi21@gmail.com>
2025-06-19 15:15:42 -0700

    Update read_protein.py

--- naufal/read_protein.py
+++ naufal/read_protein.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # Path to the file
-FILE = "/data/summer2020/naufal/esm3_embeddings/CYB_CROAT.pt"
+FILE = "/data/summer2020/naufal/esm3_embeddings/COG6_ASPCL.pt"
 
 # Load the tensor
 if not os.path.exists(FILE):
