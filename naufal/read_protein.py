56cbc36f5f77c3357bc7ac70a187bd192f303da5
Naufal Alavi <naufal.alavi21@gmail.com>
2025-06-24 11:38:53 -0700

    Update read_protein.py

--- naufal/read_protein.py
+++ naufal/read_protein.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # Path to the file
-FILE = "/data/archives/naufal/final_embeddings/HPPA_XANCP.pt"
+FILE = "/data/archives/naufal/final_embeddings/PF2R_MOUSE.pt"
 
 # Load the tensor
 if not os.path.exists(FILE):
