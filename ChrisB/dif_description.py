21a825cb1c3dc07878116f57c462d382aa251fdf
ChristopherDSBarker <164856160+ChristopherDSBarker@users.noreply.github.com>
2025-06-24 00:56:41 -0700

    Update dif_description.py
    updated for describe rating

--- ChrisB/dif_description.py
+++ ChrisB/dif_description.py
@@ -1,4 +1,15 @@
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-score = util.cos_sim(model.encode(desc1), model.encode(desc2))
+
+def similarity_score(desc1, desc2):
+    emb1 = model.encode(desc1, convert_to_tensor=True)
+    emb2 = model.encode(desc2, convert_to_tensor=True)
+    score = util.cos_sim(emb1, emb2)
+    return score.item()
+
+# Example test run (can remove in production)
+if __name__ == "__main__":
+    desc_generated = "Protein acts as a transporter of ions."
+    desc_reference = "This protein functions in ion transport across membranes."
+    print("Similarity:", similarity_score(desc_generated, desc_reference))
