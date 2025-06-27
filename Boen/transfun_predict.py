ff33464a32d9e704d1661cd0a9823fb41bd1e521
Boen Liu <boen19906@gmail.com>
2025-06-27 12:41:17 -0700

    Create transfun_predict.py

---
+++ Boen/transfun_predict.py
@@ -0,0 +1,252 @@
+import argparse
+import os
+
+import networkx as nx
+import obonet
+import torch
+from Bio import SeqIO
+from torch import optim
+from torch_geometric.loader import DataLoader
+import Constants
+import params
+from Dataset.Dataset import load_dataset
+from models.gnn import GCN
+from preprocessing.utils import load_ckp, get_sequence_from_pdb, create_seqrecord, get_proteins_from_fasta, \
+    generate_bulk_embedding, pickle_load, fasta_to_dictionary
+
+parser = argparse.ArgumentParser(description=" Predict protein functions with TransFun ", epilog=" Thank you !!!")
+parser.add_argument('--data-path', type=str, default="data", help="Path to data files")
+parser.add_argument('--ontology', type=str, default="cellular_component", help="Path to data files")
+parser.add_argument('--no-cuda', default=False, help='Disables CUDA training.')
+parser.add_argument('--batch-size', default=10, help='Batch size.')
+parser.add_argument('--input-type', choices=['fasta', 'pdb'], default="fasta",
+                    help='Input Data: fasta file or PDB files')
+parser.add_argument('--fasta-path', default="sequence.fasta", help='Path to Fasta')
+parser.add_argument('--pdb-path', default="alphafold", help='Path to directory of PDBs')
+parser.add_argument('--cut-off', type=float, default=0.0, help="Cut of to report function")
+parser.add_argument('--output', type=str, default="output", help="File to save output")
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+if args.cuda:
+    device = 'cuda'
+else:
+    device = 'cpu'
+
+if args.ontology == 'molecular_function':
+    ont_kwargs = params.mol_kwargs
+elif args.ontology == 'cellular_component':
+    ont_kwargs = params.cc_kwargs
+elif args.ontology == 'biological_process':
+    ont_kwargs = params.bio_kwargs
+ont_kwargs['device'] = device
+
+FUNC_DICT = {
+    'cellular_component': 'GO:0005575',
+    'molecular_function': 'GO:0003674',
+    'biological_process': 'GO:0008150'
+}
+
+print("Predicting proteins")
+
+def create_fasta(proteins):
+    fasta = []
+    for protein in proteins:
+        # Try both .pdb.gz and .pdb extensions
+        pdb_path_gz = "{}/{}/{}.pdb.gz".format(args.data_path, args.pdb_path, protein)
+        pdb_path = "{}/{}/{}.pdb".format(args.data_path, args.pdb_path, protein)
+        
+        if os.path.exists(pdb_path_gz):
+            alpha_fold_seq = get_sequence_from_pdb(pdb_path_gz, "A")
+        elif os.path.exists(pdb_path):
+            alpha_fold_seq = get_sequence_from_pdb(pdb_path, "A")
+        else:
+            print(f"Warning: PDB file not found for protein {protein} (tried both .pdb and .pdb.gz)")
+            continue
+            
+        fasta.append(create_seqrecord(id=protein, seq=alpha_fold_seq))
+    
+    SeqIO.write(fasta, "{}/sequence.fasta".format(args.data_path), "fasta")
+    args.fasta_path = "{}/sequence.fasta".format(args.data_path)
+
+
+def write_to_file(data, output):
+    with open('{}'.format(output), 'w') as fp:
+        for protein, go_terms in data.items():
+            for go_term, score in go_terms.items():
+                fp.write('%s %s %s\n' % (protein, go_term, score))
+
+
+def generate_embeddings(fasta_path):
+    def merge_pts(keys, fasta):
+        embeddings = [0, 32, 33]
+        for protein in keys:
+            fasta_dic = fasta_to_dictionary(fasta)
+            tmp = []
+            for level in range(keys[protein]):
+                os_path = "{}/esm/{}_{}.pt".format(args.data_path, protein, level)
+                tmp.append(torch.load(os_path))
+
+            data = {'representations': {}, 'mean_representations': {}}
+            for index in tmp:
+                for rep in embeddings:
+                    assert torch.equal(index['mean_representations'][rep], torch.mean(index['representations'][rep], dim=0))
+
+                    if rep in data['representations']:
+                        data['representations'][rep] = torch.cat((data['representations'][rep], index['representations'][rep]))
+                    else:
+                        data['representations'][rep] = index['representations'][rep]
+
+            for emb in embeddings:
+                assert len(fasta_dic[protein][3]) == data['representations'][emb].shape[0]
+
+            for rep in embeddings:
+                data['mean_representations'][rep] = torch.mean(data['representations'][rep], dim=0)
+
+            torch.save(data, "{}/esm/{}.pt".format(args.data_path, protein))
+
+    def crop_fasta(record):
+        splits = []
+        keys = {}
+        main_id = record.id
+        keys[main_id] = int(len(record.seq) / 1021) + 1
+        for pos in range(int(len(record.seq) / 1021) + 1):
+            id = "{}_{}".format(main_id, pos)
+            seq = str(record.seq[pos * 1021:(pos * 1021) + 1021])
+            splits.append(create_seqrecord(id=id, name=id, description="", seq=seq))
+        return splits, keys
+
+    keys = {}
+    sequences = []
+    input_seq_iterator = SeqIO.parse(fasta_path, "fasta")
+    for record in input_seq_iterator:
+        if len(record.seq) > 1021:
+            _seqs, _keys = crop_fasta(record)
+            sequences.extend(_seqs)
+            keys.update(_keys)
+        else:
+            sequences.append(record)
+
+    cropped_fasta = "{}/sequence_cropped.fasta".format(args.data_path)
+    SeqIO.write(sequences, cropped_fasta, "fasta")
+
+    generate_bulk_embedding("./preprocessing/extract.py", "{}".format(cropped_fasta),
+                            "{}/esm".format(args.data_path))
+
+    # merge
+    if len(keys) > 0:
+        print("Found {} protein with length > 1021".format(len(keys)))
+        merge_pts(keys, fasta_path)
+
+
+# Initialize proteins list
+proteins = []
+
+if args.input_type == 'fasta':
+    if not args.fasta_path is None:
+        proteins = set(get_proteins_from_fasta("{}/{}".format(args.data_path, args.fasta_path)))
+        # Get all PDB files (both .pdb and .pdb.gz)
+        pdb_files = os.listdir("{}/{}".format(args.data_path, args.pdb_path))
+        pdbs = set()
+        for pdb_file in pdb_files:
+            if pdb_file.endswith(".pdb.gz"):
+                pdbs.add(pdb_file.replace(".pdb.gz", ""))
+            elif pdb_file.endswith(".pdb"):
+                pdbs.add(pdb_file.replace(".pdb", ""))
+        
+        proteins = list(pdbs.intersection(proteins))
+        
+elif args.input_type == 'pdb':
+    if not args.pdb_path is None:
+        pdb_path = "{}/{}".format(args.data_path, args.pdb_path)
+        if os.path.exists(pdb_path):
+            pdb_files = os.listdir(pdb_path)
+            proteins = []
+            for pdb_file in pdb_files:
+                if pdb_file.endswith(".pdb.gz"):
+                    proteins.append(pdb_file.replace(".pdb.gz", ""))
+                elif pdb_file.endswith(".pdb"):
+                    proteins.append(pdb_file.replace(".pdb", ""))
+            
+            if len(proteins) == 0:
+                print("No proteins found in {}.".format(pdb_path))
+                exit()
+            create_fasta(proteins)
+        else:
+            print("PDB directory not found -- {}".format(pdb_path))
+            exit()
+
+if len(proteins) > 0:
+    print("Predicting for {} proteins".format(len(proteins)))
+else:
+    print("No proteins found for prediction.")
+    exit()
+
+print("Generating Embeddings from {}".format(args.fasta_path))
+os.makedirs("{}/esm".format(args.data_path), exist_ok=True)
+generate_embeddings(args.fasta_path)
+
+kwargs = {
+    'seq_id': Constants.Final_thresholds[args.ontology],
+    'ont': args.ontology,
+    'session': 'selected',
+    'prot_ids': proteins,
+    'pdb_path': "{}/{}".format(args.data_path, args.pdb_path)
+}
+
+dataset = load_dataset(root=args.data_path, **kwargs)
+
+test_dataloader = DataLoader(dataset,
+                             batch_size=args.batch_size,
+                             drop_last=False,
+                             shuffle=False)
+
+# model
+model = GCN(**ont_kwargs)
+model.to(device)
+
+optimizer = optim.Adam(model.parameters())
+
+ckp_pth = "{}/{}.pt".format(args.data_path, args.ontology)
+print(ckp_pth)
+# load the saved checkpoint
+if os.path.exists(ckp_pth):
+    model, optimizer, current_epoch, min_val_loss = load_ckp(ckp_pth, model, optimizer, device)
+else:
+    print("Model not found. Skipping...")
+    exit()
+
+model.eval()
+
+scores = []
+proteins = []
+
+for data in test_dataloader:
+    with torch.no_grad():
+        proteins.extend(data['atoms'].protein)
+        scores.extend(model(data.to(device)).tolist())
+
+assert len(proteins) == len(scores)
+
+goterms = pickle_load('{}/go_terms'.format(args.data_path))[f'GO-terms-{args.ontology}']
+go_graph = obonet.read_obo(open("{}/go-basic.obo".format(args.data_path), 'r'))
+go_set = nx.ancestors(go_graph, FUNC_DICT[args.ontology])
+
+results = {}
+for protein, score in zip(proteins, scores):
+    protein_scores = {}
+
+    for go_term, _score in zip(goterms, score):
+        if _score > args.cut_off:
+            protein_scores[go_term] = max(protein_scores.get(go_term, 0), _score)
+
+    for go_term, max_score in list(protein_scores.items()):
+        descendants = nx.descendants(go_graph, go_term).intersection(go_set)
+        for descendant in descendants:
+            protein_scores[descendant] = max(protein_scores.get(descendant, 0), max_score)
+
+    results[protein] = protein_scores
+
+print("Writing output to {}".format(args.output))
+write_to_file(results, "{}/{}".format(args.data_path, args.output))
