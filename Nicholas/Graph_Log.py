c6dce8acbc9c464f36536c6bc1b578d7a5542354
Person284 <146592850+Person284@users.noreply.github.com>
2025-06-27 23:55:03 -0700

    Update Graph_Log.py

--- Nicholas/Graph_Log.py
+++ Nicholas/Graph_Log.py
@@ -5,9 +5,9 @@ df = pd.read_csv('training_log_test.csv')
 df.plot( x = 'episode', y = 'reward')
 plt.savefig('training_log.png')
 
-ef = pd.read_csv('eval_log_125000.csv')
+ef = pd.read_csv('eval_log.csv')
 
 ef.plot( x = 'episode', y = 'avg_reward')
 
-plt.savefig('eval_log.png')
+plt.savefig('eval_log_125000.png')
 plt.show()
