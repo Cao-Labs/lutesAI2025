249033bdb80e19b3c17abc379c08dcf7755541e3
Naufal Alavi <naufal.alavi21@gmail.com>
2025-07-01 19:44:01 -0700

    Update read_protein.py

--- naufal/read_protein.py
+++ naufal/read_protein.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # Path to the file
-FILE = "/data/summer2020/naufal/testing_normalized/SYS_BACC7.pt"
+FILE = "/data/summer2020/naufal/testing_normalized/COG6_ASPCL.pt"
 
 # Load the tensor
 if not os.path.exists(FILE):
