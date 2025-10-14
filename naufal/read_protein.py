f3faf3ce3348df58430614871b0fa842e03e3d29
Naufal Alavi <naufal.alavi21@gmail.com>
2025-10-14 15:16:30 -0700

    Update read_protein.py

--- naufal/read_protein.py
+++ naufal/read_protein.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # Path to the file
-FILE = "/data/summer2020/naufal/testing_normalized/NDL1_ARATH.pt"
+FILE = "/data/archives/naufal/final_embeddings/NDL1_ARATH.pt"
 
 # Load the tensor
 if not os.path.exists(FILE):
